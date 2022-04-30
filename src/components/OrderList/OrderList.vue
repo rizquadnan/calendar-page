d1786aa893366134220cb172c3a7f586114205f5
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-04-30 11:23:33 +0700

    feat: order list component

---
+++ src/components/OrderList/OrderList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="px-4">
+    <li class="py-4">
+      <div class="d-flex justify-space-between align-center">
+        <span class="text-body-1 font-weight-bold">Nestle - Milo</span>
+        <span class="caption">Unpaid</span>
+      </div>
+    </li>
+    <li class="py-4">
+      <div class="d-flex justify-space-between align-center">
+        <span class="text-body-1 font-weight-bold">Godiva Chocolatier</span>
+        <span class="caption">Unpaid</span>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  border-bottom: 1px solid #dadada;
+}
+</style>
