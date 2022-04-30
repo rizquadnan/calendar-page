5f5009e484755352570a008a79fc7f95f0fba71e
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-04-30 11:25:50 +0700

    feat: done slicing page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,48 @@
 <template>
   <v-app>
-    <v-main>
-      <Calendar
-        :initialDate="mockData.calendar.initialDate"
-        :blockedDates="mockData.calendar.blockedDates"
-        :reservedDates="mockData.calendar.reservedDates"
-        :onChangeDate="onChangeDate"
-      />
-    </v-main>
+    <div class="app">
+      <div class="hero">
+        <div class="app-wrapper">
+          <header
+            class="d-flex justify-space-between align-center mb-2 pl-4 pr-3"
+          >
+            <h1 class="text-h5 font-weight-bold">Calendar</h1>
+            <v-btn icon>
+              <v-icon aria-label="Next Month" role="button" aria-hidden="false">
+                mdi-cog-outline
+              </v-icon>
+            </v-btn>
+          </header>
+          <div class="calendar-wrapper">
+            <Calendar
+              :initialDate="mockData.calendar.initialDate"
+              :blockedDates="mockData.calendar.blockedDates"
+              :reservedDates="mockData.calendar.reservedDates"
+              :onChangeDate="onChangeDate"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="app-wrapper">
+        <OrderList />
+      </div>
+      <AppFooter />
+    </div>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Calendar } from "@/components";
+import OrderList from "./components/OrderList/OrderList.vue";
+import AppFooter from "./components/AppFooter/AppFooter.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
     Calendar,
+    OrderList,
+    AppFooter,
   },
   data: () => ({
     mockData: {
@@ -44,3 +68,23 @@ export default Vue.extend({
   },
 });
 </script>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+}
+.app-wrapper {
+  max-width: 375px;
+  margin: 0 auto;
+}
+
+.hero {
+  background: #f4f4f4;
+  padding-top: 48px;
+}
+.calendar-wrapper {
+  max-width: 327px;
+  min-height: 346px;
+  margin: 0 auto;
+}
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export * from "./Calendar";
+export * from "./OrderList";
+export * from "./AppFooter";
